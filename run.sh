5121246e25682e3fa03169f554f1dd41d36b4a0e
falk <>
2000-12-12 13:52:14 +0000

    Keep Size.hh if it didn't change to avoid rebuilding.

--- run.sh
+++ run.sh
@@ -6,6 +6,13 @@ line=`grep $level stats`
 unique=`echo "$line" | cut -d'|' -f 4 | tr ' ' '0' | sed 's,^0*\([^0]\),\1,'`
 paired=`echo "$line" | cut -d'|' -f 5 | tr ' ' '0' | sed 's,^0*\([^0]\),\1,'`
 echo $unique $paired
-cat Size.hh.in | sed s,@unique@,$unique, | sed s,@paired@,$paired, > Size.hh
+
+cat Size.hh.in | sed s,@unique@,$unique, | sed s,@paired@,$paired, > Size.hh.tmp
+if cmp -s Size.hh Size.hh.tmp; then
+  rm Size.hh.tmp
+else
+  mv Size.hh.tmp Size.hh
+fi
+
 make
 ./atomixer $1
