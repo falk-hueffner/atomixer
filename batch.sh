dbe174c16d53ce7497507e08a5a59c51a3b02d3c
Falk Hüffner <falk-hueffner@users.noreply.github.com>
2000-12-12 14:30:10 +0000

    Removed bogus adrien_02 solution.

--- batch.sh
+++ batch.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
-for i in `cat stats | awk '{print levels/$1}'`; do
+for i in `cat stats | awk '{print "levels/"$1}'`; do
     if [ -f $i ]; then
-        ./run.sh $i
+	level=`basename $i`
+	line=`grep $level stats`
+	solved=`echo "$line" | cut -d'|' -f 11`
+	if [ $solved ]; then
+	    echo $level: already solved in $solved moves
+	else
+	    ./run.sh $i
+	fi
     fi
 done
