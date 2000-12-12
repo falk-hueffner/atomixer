a8a78683a4baed7d30c674a31baa3838fc876f2f
falk <>
2000-12-12 14:35:32 +0000

    Correct calculation of $paired.

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 level=`basename $1`
 echo -n "$level: "
 line=`grep $level stats`
-unique=`echo "$line" | cut -d'|' -f 4 | tr ' ' '0' | sed 's,^0*\([^0]\),\1,'`
-paired=`echo "$line" | cut -d'|' -f 5 | tr ' ' '0' | sed 's,^0*\([^0]\),\1,'`
+unique=`echo "$line" | cut -d'|' -f 4 | tr ' ' '0'`
+unique=`echo $unique \* 1 | bc`
+paired=`echo "$line" | cut -d'|' -f 5 | tr ' ' '0'`
+paired=`echo $paired \* 2 | bc`
 echo $unique $paired
 
 cat Size.hh.in | sed s,@unique@,$unique, | sed s,@paired@,$paired, > Size.hh.tmp
