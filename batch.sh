8847c77b8adc1c51e464514f50a0248b3aba0cdf
Falk Hüffner <falk-hueffner@users.noreply.github.com>
2000-12-12 13:27:01 +0000

    Just call run.sh for all arguments.

--- batch.sh
+++ batch.sh
@@ -1,13 +1,5 @@
 #! /bin/zsh
 
 for i in levels/*; do
-    level=`basename $i`
-    echo -n "$level: "
-    line=`grep $level stats`
-    unique=`echo "$line" | cut -d'|' -f 4 | tr ' ' '0' | sed 's,^0*\([^0]\),\1,'`
-    paired=`echo "$line" | cut -d'|' -f 5 | tr ' ' '0' | sed 's,^0*\([^0]\),\1,'`
-    echo $unique $paired
-    cat Size.hh.in | sed s,@unique@,$unique, | sed s,@paired@,$paired, > Size.hh
-    make
-    ./atomixer $i
+    ./run.sh $i
 done
