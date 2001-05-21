f041b74a6189f1a04a83b32a9a8d932a4d13b1d0
falk <>
2001-05-21 15:49:40 +0000

    Snarf board size from level file and set LARGE_BOARD if necessary.

--- run.sh
+++ run.sh
@@ -13,12 +13,15 @@ fi
 
 set `awk -f countatoms.awk < "$level"`
 
-echo $1 unique, $2 paired, and $3 multiples.
+echo "$1 x $2 board, large: $3; $4 unique, $5 paired, and $6 multiples."
 
 cat Size.hh.in | \
-   sed s,@unique@,$1, | \
-   sed s,@paired@,$2, | \
-   sed s,@multi@,$3, > Size.hh.tmp
+   sed s,@xsize@,$1, | \
+   sed s,@ysize@,$2, | \
+   sed s,@large_board@,$3, | \
+   sed s,@unique@,$4, | \
+   sed s,@paired@,$5, | \
+   sed s,@multi@,$6, > Size.hh.tmp
 
 if cmp -s Size.hh Size.hh.tmp; then
     rm Size.hh.tmp
