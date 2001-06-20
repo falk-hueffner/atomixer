231428518b01ac92b97b9cb22eb7bb3ea94abb8a
falk <>
2001-06-20 22:18:38 +0000

    Don't fiddle with stats, but simply read levels from stdin.

--- batch.sh
+++ batch.sh
@@ -2,17 +2,8 @@
 
 exec 6>batch.$$.log
 
-for i in `cat stats | awk '{print "levels/"$1}'`; do
-    if [ -f $i ]; then
-	level=`basename $i`
-	line=`grep $level stats`
-	#solved=`echo "$line" | cut -d'|' -f 11`
-	if [ $solved ]; then
-	    echo $level: already solved in $solved moves
-	else
-	    printf "$level: started on `date`" >&6
-	    ./run.sh $i
-	    printf " finished on `date`\n" >&6
-	fi
-    fi
+while read level; do
+    printf "$level: started on `date`" >&6
+    ./run.sh "$level"
+    printf " finished on `date`\n" >&6
 done
